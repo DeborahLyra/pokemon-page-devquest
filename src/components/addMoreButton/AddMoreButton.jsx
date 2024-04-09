9a6626536422ee46c17968cc89360166adcdc55a
Deborah Lyra <deborahp.lyra@gmail.com>
2024-04-09 20:54:35 -0300

    ajustes css

--- src/components/addMoreButton/AddMoreButton.jsx
+++ src/components/addMoreButton/AddMoreButton.jsx
@@ -1,10 +1,7 @@
-import React, { useContext } from 'react'
-import { ThemeContext, themes } from '../../contexts/theme-context'
 import { Button } from '../button/Button.jsx'
 
 export function AddMoreButton({ onClick }) {
 
-  const { theme, setTheme } = useContext(ThemeContext)
 
   return (
     <div>
